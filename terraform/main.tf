8e387449e14475ad18c2c73f88ff503c85906ddc
Pujan Shrestha <pujanshrestha240@gmail.com>
2026-02-15 15:28:44 +0545

    Fix: error on main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_versioning" "tf_state" {
     bucket = aws_s3_bucket.tf_state.id   
 
     versioning_configuration {
-      status = disabled
+      status = "Disabled"
     }
 }
 
