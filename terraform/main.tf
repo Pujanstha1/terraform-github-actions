fd54e9490647e266a1cf3fbfcd8bf38cd35b844a
Pujan Shrestha <pujanshrestha240@gmail.com>
2026-02-15 09:49:30 +0545

    Add: Initial codes

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+    bucket = "adex_terraform_9842463240"
+}
+
+resource "aws_s3_bucket_versioning" "tf_state" {
+    bucket = aws_s3_bucket.tf_state.id   
+
+    versioning_configuration {
+      status = disabled
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+    bucket = aws_s3_bucket.tf_state.id 
+
+    block_public_acls = true 
+    block_public_policy = true 
+    ignore_public_acls = true 
+    restrict_public_buckets = true 
+}
